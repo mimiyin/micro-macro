38a44d68b00b884700d422bf039b6307489bc18c
Mimi Yin <mimiyin@gmail.com>
2026-06-12 12:56:26 -0400

    words sketch.

---
+++ micro/words/sketch.js
@@ -0,0 +1,42 @@
+let phrases = [];
+let prompter;
+
+let urlParams = new URLSearchParams(window.location.search);
+
+// SETTINGS
+let src = parseInt(urlParams.get('src')) || 'words';
+
+function preload() {
+  phrases = loadStrings(src +'.txt');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  prompter = new p5.Speech(); // speech synthesis object
+}
+
+function draw() {
+  // if (frameCount % 300 == 1) {
+  //   let random_phrase = random(phrases);
+  //   prompter.speak(random_phrase);
+  //   textSize(48);
+  //   textAlign(CENTER);
+  //   background(0);
+  //   fill('white');
+  //   text(random_phrase, width / 2, height / 2);
+  // }
+}
+
+function next() {
+  let random_phrase = random(phrases);
+    prompter.speak(random_phrase);
+    textSize(48);
+    textAlign(CENTER);
+    background(0);
+    fill('white');
+    text(random_phrase, width / 2, height / 2);
+}
+
+function keyPressed() {
+  next();
+}
