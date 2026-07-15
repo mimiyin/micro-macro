a34df2a5ff713ee01a64b0057b7f42e59507e0b4
Mimi Yin <mimiyin@gmail.com>
2026-07-15 15:32:56 -0400

    New words - combined speakers.

--- micro/words/sketch.js
+++ micro/words/sketch.js
@@ -1,5 +1,6 @@
-let phrases = [];
-let prompter;
+let ab = { 'a' : [], 'b': [] };
+let phrases = structuredClone(ab);;
+let speakers = structuredClone(ab);
 
 let urlParams = new URLSearchParams(window.location.search);
 
@@ -7,37 +8,54 @@ let urlParams = new URLSearchParams(window.location.search);
 let src = urlParams.get('src') || 'words';
 console.log(src);
 
+// RECORD
+let record = structuredClone(ab);
+let timers = structuredClone(ab);
+
 function preload() {
-  phrases = loadStrings(src + '.txt');
+  for(let l in ab) phrases[l] = loadStrings(l + '.txt');
+  //timers = loadJSON('record.json');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  prompter = new p5.Speech(); // speech synthesis object
+  for(let l in ab) {
+    speakers[l] = new p5.Speech(); // speech synthesis object
+    speakers[l].setPitch(l == 'a' ? 0.5 : 2);
+  }
+  background(128);
 }
 
 function draw() {
-  // if (frameCount % 300 == 1) {
-  //   let random_phrase = random(phrases);
-  //   prompter.speak(random_phrase);
-  //   textSize(48);
-  //   textAlign(CENTER);
-  //   background(0);
-  //   fill('white');
-  //   text(random_phrase, width / 2, height / 2);
-  // }
+  // auto-pilot;
+  for(let ab in timers) {
+    let timer = timers[ab];
+    for(let fc of timer) {
+      if(frameCount > fc) {
+        next(ab);
+        break;
+      }
+    }
+  }
 }
 
-function next() {
-  let random_phrase = random(phrases);
-    prompter.speak(random_phrase);
-    textSize(48);
-    textAlign(CENTER);
-    background(0);
-    fill('white');
-    text(random_phrase, width / 2, height / 2);
+function next(k) {
+  let random_phrase = random(phrases[k]);
+  speakers[k].speak(random_phrase);
+  textSize(48);
+  textAlign(CENTER);
+  
+  let x = key == 'a' ? 0 : width/2;
+  fill(x == 0 ? 33 : 66);
+  rect(x, 0, width/2, height);
+  fill('white');
+  text(random_phrase, x + (width/4), height / 2);
+
+  // log it
+  record[k].push(frameCount);
 }
 
 function keyPressed() {
-  next();
+  if (key in record) next(key);
+  else if(keyCode == ENTER) saveJSON(record, 'record-'+ Date.now() + '.json');
 }
